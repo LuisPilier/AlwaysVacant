61f08fbdfc40558fa8229db9f6506a135facab54
AVCode <38996897+advr0106@users.noreply.github.com>
2021-02-02 00:48:01 -0400

    ..

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
 @NgModule({
   declarations: [
     AppComponent,
