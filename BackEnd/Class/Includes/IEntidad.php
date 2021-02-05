8df8325a2e668585e6f40132e92ef024d910939e
CDIM17 <davidinojosa5@gmail.com>
2021-02-04 23:56:05 -0400

    Update Interface
    Agregando parametro JSON en los metodos de la interface

--- BackEnd/Class/Includes/IEntidad.php
+++ BackEnd/Class/Includes/IEntidad.php
@@ -5,10 +5,10 @@ include('Conexion.php');
 interface IEntidad
 {
     function Guardar($conn,$json);
-    static function ObtenerTodo($conn);
-    static function Obtener($conn,$id);
-    function Actualizar($conn,$id);
-    static function Eliminar($conn,$id);
+    static function ObtenerTodo($conn,$json);
+    static function Obtener($conn,$json);
+    function Actualizar($conn,$json);
+    static function Eliminar($conn,$json);
     
 }
 
