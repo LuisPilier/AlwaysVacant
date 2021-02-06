ff194fac03a59aa92918386b56cc21eb801ecaa8
CDIM17 <davidinojosa5@gmail.com>
2021-02-05 23:25:55 -0400

    Update Vacante.php

--- BackEnd/Class/Vacante.php
+++ BackEnd/Class/Vacante.php
@@ -9,23 +9,76 @@ class Vacante implements IEntidad
         private $Compania;
         private $Logo;
         private $URL;
-        private $Position;
+        private $Posicion;
         private $Descripcion;
         private $ID_Categoria;
         private $ID_Tipo_Vacante;
         private $ID_Ciudad; 
+        private $Ubicacion;
         private $Token;
+        private $Email;
+        private $Table = 'Vacante';
 
-        public function Guardar($conn)
+        public function Guardar($conn,$json)
         {
-            echo '';
+            $datos = json_decode($json,true);
+
+            if(!isset($datos['Compania']) || !isset($datos['ID_Tipo_Vacante']) || !isset($datos['Posicion']) || !isset($datos['ID_Ciudad']) || !isset($datos['Ubicacion']) || !isset($datos['ID_Categoria']))
+            {
+                return respuestas::error_400();
+            }
+            else
+            {
+                $this->Compania        = $datos['Compania'];
+                $this->ID_Tipo_Vacante = $datos['ID_Tipo_Vacante'];
+                $this->Posicion        = $datos['Posicion'];
+                $this->ID_Ciudad       = $datos['ID_Ciudad'];
+                $this->Ubicacion       = $datos['Ubicacion'];
+                $this->ID_Categoria    = $datos['ID_Categoria'];
+                $this->Email           = isset($datos['Email']) ? $datos['Email'] : '';
+                $this->Logo            = isset($datos['Logo'])  ? $datos['Logo']  : '';
+                $this->URL             = isset($datos['URL'])   ? $datos['URL']   : '';
+                
+                $resp = $this->insertar_vacante($conn);
+
+                if($resp)
+                {
+                    $respuesta = respuestas::$response;
+                    $respuesta['result'] = array(
+                        "ID_Vacante" => $resp
+                    );
+                    return $respuesta;
+                }
+                else
+                {
+                    //return respuestas::error_500();
+                    return $resp;
+                }
+            }
+
         }
 
         private function insertar_vacante($conn)
         {
-            $query = "INSERT INTO ".$this->table. " (DNI,Nombre,Direccion,CodigoPostal,Telefono,Genero,FechaNacimiento,Correo,Imagen) VALUES('".$this->dni."','".$this->nombre."','".$this->direccion."','".$this->codigoPostal."', '".$this->telefono."','".$this->genero."','".$this->fechaNacimiento."', '".$this->correo."','".$this->imagen."')";
-        
-            $resp = $conn->nonQueryId($query);
+            $insert_fields = array(
+             'Compania'        => "'$this->Compania'",
+             'Logo' 	       => "'$this->Logo'",
+             'URL'             => "'$this->URL'",
+             'Posicion'        => "'$this->Posicion'",
+             'Descripcion'     => "'$this->Descripcion'",
+             'ID_Ciudad'       => "'$this->ID_Ciudad'",
+             'ID_Categoria'    => "'$this->ID_Categoria'",
+             'ID_Tipo_Vacante' => "'$this->ID_Tipo_Vacante'",
+             'Email'           => "'$this->Email'",
+             'Ubicacion'       => "'$this->Ubicacion'"
+             );
+    
+            // Insert record
+            $insert_sql = 'INSERT INTO ' . $this->Table
+                . ' ('   . implode(', ', array_keys($insert_fields))   . ')'
+                . ' VALUES ('    . implode(', ', array_values($insert_fields)) . ')';
+    
+            $resp = $conn->nonQueryId($insert_sql);
     
             if($resp)
             {
@@ -34,25 +87,26 @@ class Vacante implements IEntidad
             else
             {
                 return 0;
+                //return $insert_sql;
             }
         }
 
-        public static function ObtenerTodo($conn)
+        public static function ObtenerTodo($conn,$json)
         {
             echo '';
         }
 
-        public static function Obtener($conn,$id)
+        public static function Obtener($conn,$json)
         {
             echo '';
         }
 
-        public function Actualizar($conn,$id)
+        public function Actualizar($conn,$json)
         {
             echo '';
         }
 
-        public static function Eliminar($conn,$id)
+        public static function Eliminar($conn,$json)
         {
             echo '';
         }
@@ -60,7 +114,9 @@ class Vacante implements IEntidad
 
 }
 
+//$v1 = new Vacante();
 
+//$v1->Guardar('','');
 
 
 
