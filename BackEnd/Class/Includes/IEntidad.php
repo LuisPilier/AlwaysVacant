849a6bb24fb148f3dcd683b9b8302afd4c419df5
CDIM17 <davidinojosa5@gmail.com>
2021-02-08 02:06:57 -0400

    Update IEntidad.php

--- BackEnd/Class/Includes/IEntidad.php
+++ BackEnd/Class/Includes/IEntidad.php
@@ -8,11 +8,7 @@ interface IEntidad
     static function ObtenerTodo($conn,$array);
     static function Obtener($conn,$array);
     function Actualizar($conn,$json);
-<<<<<<< HEAD
     function Eliminar($conn,$json);
-=======
-    function Eliminar($conn,$array);
->>>>>>> 9a240b64040ea82485f3bab7c23efb060c58c6c3
     
 }
 
