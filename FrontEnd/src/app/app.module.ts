2c3df1dec972d3a3ac83dde33a218a60dc884a3a
AVCode <38996897+advr0106@users.noreply.github.com>
2021-02-10 01:50:20 -0400

    Repairing header

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminpageComponent } from './components/adminpage/adminpage.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { PagepostjobComponent } from './components/pagepostjob/pagepostjob.component';
@@ -23,9 +22,7 @@ import { PagepostjobComponent } from './components/pagepostjob/pagepostjob.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     NoopAnimationsModule,
-   
   ],
   providers: [],
   bootstrap: [AppComponent]
