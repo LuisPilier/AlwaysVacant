4e1453e7f03b0fdc6dad9eafa4af73b10690f0ad
SamuelPerez27 <samuelpperez07@gmail.com>
2021-02-15 02:04:11 -0400

    Update Categeria(implementado Token)

--- BackEnd/API/categoria.php
+++ BackEnd/API/categoria.php
@@ -4,41 +4,46 @@
 //Header
 header("Content-Type: application/json");
 
-//Include
+//Include de la clase Categoria para utilizar los metodos
 include('../Class/Categoria.php');
 
-
+//Instancia
 $_categoria= new categoria();
 
 //Switch(Desicion)
-
 switch($_SERVER['REQUEST_METHOD']){
 
 
     case 'GET':
-      
+        //Recibiendo los datos del JSON
         $array = json_decode(file_get_contents("php://input"),true);
-
+       
+        //Si recibimos el ID de la categoria
         if(isset($array['ID_Categoria']))
         {
+            //Retorna la categoria especificada por el ID
           $listaCategoria = $_categoria->Obtener($conn,$array);
         }
         else
         {
+            //Retorna todas las categorias que se encuentren registradas
           $listaCategoria = $_categoria->ObtenerTodo($conn,$array);
         }
         
-    
+        //Mostrar el contenido
         echo json_encode($listaCategoria);
 
         break;
     
     case 'POST':
-        
+             
+            //Recibiendo los datos del JSON
             $postBody = file_get_contents("php://input");
-                    
+            
+            //Llamando al metodo guardar que se encuentra en la clase Categoria        
             $datosArray = $_categoria->Guardar($conn,$postBody);
 
+            // Si existe algun error al guardar la categoria
             if (isset($datosArray["result"]["error_id"])) {
                 // code...
                 $responseCode = $datosArray["result"]["error_id"];
@@ -48,6 +53,7 @@ switch($_SERVER['REQUEST_METHOD']){
                 http_response_code(200);
             }
 
+            //Mostrar el contenido
             echo json_encode($datosArray);
 
 
@@ -56,11 +62,14 @@ switch($_SERVER['REQUEST_METHOD']){
 
 
     case 'PUT':
-           
+         
+        //Recibiendo los datos del JSON 
         $postBody = file_get_contents("php://input");
                     
+        //Llamando al metodo actualizar que se encuentra en la clase Categoria 
         $datosArray = $_categoria->Actualizar($conn,$postBody);
 
+         // Si existe algun error al actualizar la categoria
         if (isset($datosArray["result"]["error_id"])) {
             // code...
             $responseCode = $datosArray["result"]["error_id"];
@@ -70,16 +79,20 @@ switch($_SERVER['REQUEST_METHOD']){
             http_response_code(200);
         }
 
+        //Retornar el contenido
         echo json_encode($datosArray);
       break;   
          
          
     case 'DELETE':
         
+        //Recibiendo los datos del JSON 
         $postBody = file_get_contents("php://input");
-                    
+                  
+         //Llamando al metodo eliminar que se encuentra en la clase Categoria 
         $datosArray = $_categoria->Eliminar($conn,$postBody);
 
+          // Si existe algun error al eliminar la categoria
         if (isset($datosArray["result"]["error_id"])) {
             // code...
             $responseCode = $datosArray["result"]["error_id"];
@@ -89,6 +102,7 @@ switch($_SERVER['REQUEST_METHOD']){
             http_response_code(200);
         }
 
+        //Retornar el contenido
         echo json_encode($datosArray);
 
        break;
--- BackEnd/Class/Categoria.php
+++ BackEnd/Class/Categoria.php
@@ -1,71 +1,92 @@
 <?php
 
+//Incluyendo la interface
 include('Includes/IEntidad.php');
 
+//Clase categoria que implementa la interface
 class Categoria implements IEntidad{
 
         //Atributos
         private $Tabla = "Categoria";
         private $ID_Categoria = "";
         private $Nombre = "";
-        private $token;
+        private $token ="";
         
-
-        //Funcion de la Interface    
+         //METODOS
+         
+         
+        //Funcion implementada de la Interface    
         public function Guardar($conn,$json)
         {
             // Array con datos
             $data = json_decode($json,true);
 
-             //Comprobar campos requeridos
-            if( !isset($data['Nombre']))
-            {      
-                   //Campo requido no enviado     
-                   return Respuestas::error_400();
+            //Metodo de la clase token, valida si fue enviado o si es valido
+            $this->token = Token::validarToken($conn,$data);
+
+            //Comprobar si el token existe 
+            if(is_bool($this->token))
+            {
+
+                    
+                    //Comprobar campos requeridos
+                    if( !isset($data['Nombre']))
+                    {      
+                        //Campo requido no enviado     
+                        return Respuestas::error_400();
+                    }
+                    else
+                    {
+                        //Campo requido enviado  y asignado a los atributos 
+                        $this->Nombre         = $data['Nombre'];
+                        
+                        // Llamar funcion privada Insert_Categoria(Query de insertar categoria)
+                        $resp = $this->Insert_Categoria($conn);
+
+                        if($resp)
+                        {
+                            //Respuesta de insercion exitosa y ID insertado
+                            $respuesta['result'] = array(
+                                "ID_Categoria" => $resp
+                            );
+                            return $respuesta;
+                        }
+                        else
+                        {
+                            //Error al momento de insertar
+                            Respuestas::error_500();
+                        }
+
+                    }
             }
             else
             {
-                //Campo requido enviado  
-                  $this->Nombre         = $data['Nombre'];
-                
-                // Llamar funcion privada Insert_Categoria
-                $resp = $this->Insert_Categoria($conn);
-
-                if($resp)
-                {
-                     //Respuesta de insercion exitosa y ID insertado
-                    $respuesta['result'] = array(
-                        "ID_Categoria" => $resp
-                    );
-                    return $respuesta;
-                }
-                else
-                {
-                    //Error al momento de insertar
-                    Respuestas::error_500();
-                }
-
+                     //Error en caso de que el token sea invalido o no sea enviado
+                    return $this->token;
             }
         
         }
 
-        //Funcion privada (Query de insercion)
+        //Funcion privada (Query de insercion categoria)
         private function Insert_Categoria($conn)
         {
             
             //Query   
             $query = "Insert into $this->Tabla(Nombre) values ('$this->Nombre')";
             
-            //Lamando la funcion que esta en la interface
+            //Lamando la funcion que se encuentra en la clase conexion y que es utilizada por la interface
             $id = $conn->nonQueryId($query);
          
+
             if($id)
             {
+                //Se inserto correctamente
                return $id;
 
             } 
             else
             {
+                //Error al momento de insertar
                 return 0;
 
             }
@@ -74,89 +95,142 @@ class Categoria implements IEntidad{
 
 
 
-         
+         //Funcion Implementada de la Interface (Obtener todas las categoriass)
         public static function ObtenerTodo($conn,$json)
         {
-        
-          $query = self::Select_Categoria();
-          $datos = $conn->Query($query);
-          return $datos;   
+           //Validar token
+          $token = Token::validarToken($conn,$json);
+          
+
+          if(is_bool($token))
+          {
+             //Ejecutar funcion privada Select_Categoria
+            $query = self::Select_Categoria();
+            $datos = $conn->Query($query);
+
+            //Retornar datos
+            return $datos;   
+          }
+          else
+          {
+               //Error en caso de que el token sea invalido o no sea enviado
+              return  $token;
+          }
+
 
         }
 
+       //Funcion Implementada de la Interface (Obtener una categoria especificada por el ID_Categoria)
         public static function Obtener($conn,$json)
         {
-            $ID_Categoria = $json['ID_Categoria'];
-            $query = self::Select_Categoria(). ' WHERE ID_Categoria = ' .$ID_Categoria;
-            $datos = $conn->Query($query);
-            return $datos;
+            //Validar token
+             $token = Token::validarToken($conn,$json);
+
+             if(is_bool($token))
+             {
+                 //Extrayendo el ID_Categoria solicitado
+                $ID_Categoria = $json['ID_Categoria'];
+                
+                //Llamando la function privada Select_Categoria con la condicion de la categoria solicitada
+                $query = self::Select_Categoria(). ' WHERE ID_Categoria = ' .$ID_Categoria;
+                $datos = $conn->Query($query);
+                return $datos;
+             }
+             else
+             {
+                     //Error en caso de que el token sea invalido o no sea enviado
+                    return  $token;
+
+             }
 
         }
 
-
+        //Funcion privada (Query general para el select de categoria) 
         private static function Select_Categoria()
         {
-        
+            
+            //Query
             $query = "Select * from Categoria";
             return $query;
         }
 
+ 
+        //Funcion Implementada de la Interface (Actualizar la categoria)
         public function Actualizar($conn,$json)
         {
-                 // Array con datos
-                 $data = json_decode($json,true);
+                //Array con los datos
+                $data = json_decode($json,true);
 
-                 //Comprobar campos requerido
-                if( !isset($data['ID_Categoria']) ||  !isset($data['Nombre']))
-                {      
-                       //Campo requido no enviado     
-                       return Respuestas::error_400();
+                //Validar token
+                $this->token = Token::validarToken($conn,$data);
+
+                if(is_bool($this->token))
+                {
+
+
+                        //Comprobar campos requerido
+                        if( !isset($data['ID_Categoria']) ||  !isset($data['Nombre']))
+                        {      
+                            //Campos requidos no enviados     
+                            return Respuestas::error_400();
+                        }
+                        else
+                        {
+                            //Campos requidos enviados  
+                            $this->ID_Categoria         = $data['ID_Categoria'];
+                            $this->Nombre               = $data['Nombre'];
+                            
+                            // Llamar funcion obtener(para verificar si el ID introducido existe)
+                            $ID = self::Obtener($conn,$data);
+            
+                            if($ID)
+                            {
+                                //El ID introducido existe
+
+                                //Llamando a la funcion privada Actualizar_Categoria(Update Categoria)
+                                $resp = $this->Actualizar_Categoria($conn);
+                                
+                                $respuesta = Respuestas::$response;
+                                $respuesta['result'] = array(
+                                    "ID_Categoria" => $this->ID_Categoria,
+                                    "Nombre"       => $this->Nombre
+                                );
+                                return $respuesta;
+                
+                                
+                            }
+                            else
+                            {
+                                    //Error al momento de buscar el ID introducido(no existe)
+                                    return  Respuestas::error_500();
+                        }  
+                    }
                 }
                 else
                 {
-                    //Campo requido enviado  
-                      $this->ID_Categoria         = $data['ID_Categoria'];
-                      $this->Nombre               = $data['Nombre'];
-                    
-                    // Llamar funcion privada Insert_Categoria
-                    $ID = self::Obtener($conn,$data);
-     
-                    if($ID)
-                    {
-                         //EXISTE
-                         $resp = $this->Actualizar_Categoria($conn);
-                        
-                        $respuesta = Respuestas::$response;
-                        $respuesta['result'] = array(
-                            "ID_Categoria" => $this->ID_Categoria,
-                            "Nombre"       => $this->Nombre
-                        );
-                        return $respuesta;
-          
-                           
-                    }
-                    else
-                    {
-                             //Error al momento de insertar
-                             return  Respuestas::error_500();
-                   }  
-               }
+                        //Error en caso de que el token sea invalido o no sea enviado
+                       return $this->token;
+                }
            }  
         
+    
+           //Funcion privada (Update categoria) 
         private function Actualizar_Categoria($conn)
            {
+               //Query
                $query = "UPDATE $this->Tabla SET Nombre = '$this->Nombre' where ID_Categoria = $this->ID_Categoria";
                            
-               //Lamando la funcion que esta en la interface
-             
+               //Lamando la funcion que esta en la clase conexion y trae la interface
                 $resp =  $conn->nonQuery($query);
     
+                //Si se afectan las filas de la tabla categoria
                 if($resp >= 1)
                 {
                     return $resp;
                 }
                 else
                 {
+                    //Error
                     return 0;
                 }
     
@@ -165,59 +239,79 @@ class Categoria implements IEntidad{
    
 
     
-      
-           public function Eliminar($conn,$json)
+           //Funcion Implementada de la Interface (Eliminar una categoria)
+        public function Eliminar($conn,$json)
         {
-                // Array con datos
+              // Array con datos
             $data = json_decode($json,true);
 
-            //Comprobar campos requeridos
-           if( !isset($data['ID_Categoria']))
-           {      
-                  //Campo requido no enviado     
-                  return Respuestas::error_400();
-           }
-           else
-           {
-               //Campo requido enviado  
-                 $this->ID_Categoria         = $data['ID_Categoria'];
-               
-               // Llamar funcion privada Insert_Categoria
-               $ID = self::Obtener($conn,$data);
+            
+            //Validar token
+            $this->token = Token::validarToken($conn,$data);
 
-               if($ID)
-               {
-                    //EXISTE
-                    $resp = $this->Delete_Categoria($conn);
-                   
-                     $respuesta = Respuestas::$response;
-                     $respuesta['result'] = array(
-                         "ID_Categoria" => $this->ID_Categoria
-                     );
-                     return $respuesta;
-                      
-               }
-               else
-               {
-                   //Error al momento de insertar
-                   return  Respuestas::error_500();
-               }
-           }
+
+            if(is_bool($this->token))
+            {
+
+                    //Comprobar campos requeridos
+                if( !isset($data['ID_Categoria']))
+                {      
+                        //Campo requido no enviado     
+                        return Respuestas::error_400();
+                }
+                else
+                {
+                    //Campo requido enviado  
+                        $this->ID_Categoria         = $data['ID_Categoria'];
+                    
+                    // Llamar funcion obtener para ver si existe el ID solicitado
+                    $ID = self::Obtener($conn,$data);
+
+                    if($ID)
+                    {       //Existe
+                        
+                            //Llamando a la funcion privada Delete Categoria (Eliminar una categoria)
+                            $resp = $this->Delete_Categoria($conn);
+                        
+                            //Respuesta
+                            $respuesta = Respuestas::$response;
+                            $respuesta['result'] = array(
+                                "ID_Categoria" => $this->ID_Categoria
+                            );
+                            return $respuesta;
+                            
+                    }
+                    else
+                    {
+                        //Error al momento de eliminar
+                        return  Respuestas::error_500();
+                    }
+                }
+            }
+            else
+            {
+                //Error en caso de que el token sea invalido o no sea enviado
+                 return $this->token;
+            }
     }
+
+          //Funcion privada (Delete categoria) 
         private function Delete_Categoria($conn)
         {
             //Query   
             $query = "DELETE FROM $this->Tabla where ID_Categoria = $this->ID_Categoria";
                         
-            //Lamando la funcion que esta en la interface
+             //Lamando la funcion que esta en la clase conexion y trae la interface
             $resp = $conn->nonQuery($query);
              
+             //Si se afectan las filas de la tabla categoria
             if($resp >= 1)
             {
                 return $resp;
             }
             else
             {
+                //Error
                 return 0;
             }
 
