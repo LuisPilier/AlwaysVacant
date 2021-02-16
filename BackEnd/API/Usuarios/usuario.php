514f5acc17a2989f521ca47ae12059c450ce37d0
SamuelPerez27 <samuelpperez07@gmail.com>
2021-02-15 23:40:04 -0400

    Update Usuario(Token y comentarios)

--- BackEnd/API/Usuarios/usuario.php
+++ BackEnd/API/Usuarios/usuario.php
@@ -4,13 +4,9 @@
 header("Content-Type: application/json");
 
 //Include
-
-ini_set('display_errors',1); 
- error_reporting(E_ALL);
-
-
 include('../../Class/Usuarios/Usuario.php');
 
+//Instancia
 $_usuario = new Usuario();
 
 
@@ -20,29 +16,36 @@ switch($_SERVER['REQUEST_METHOD']){
 
 
     case 'GET':
-        
+
+        //Recibiendo los datos del JSON
         $array = json_decode(file_get_contents("php://input"),true);
 
+         //Si recibimos el ID del usuario
         if(isset($array['ID_Usuario']))
         {
+             //Retorna el usuario especificada por el ID
           $listaUsuario = $_usuario->Obtener($conn,$array);
         }
         else
         {
+            //Retorna todas los usuarios que se encuentren registrados
             $listaUsuario = $_usuario->ObtenerTodo($conn,$array);
         }
         
-    
-        echo json_encode($listaUsuario);
+        //Mostrar el contenido
+         echo json_encode($listaUsuario);
       
         break;
 
     case 'POST':
 
+        //Recibiendo los datos del JSON
         $postBody = file_get_contents("php://input");
-                    
+                   
+         //Llamando al metodo guardar que se encuentra en la clase Usuario   
         $datosArray = $_usuario->Guardar($conn,$postBody);
 
+        // Si existe algun error al guardar el usurio
         if (isset($datosArray["result"]["error_id"])) {
             // code...
             $responseCode = $datosArray["result"]["error_id"];
@@ -52,6 +55,7 @@ switch($_SERVER['REQUEST_METHOD']){
             http_response_code(200);
         }
 
+        //Mostrar el contenido
         echo json_encode($datosArray);
 
 
@@ -63,10 +67,14 @@ switch($_SERVER['REQUEST_METHOD']){
          
     case 'DELETE':
 
+        
+        //Recibiendo los datos del JSON 
         $postBody = file_get_contents("php://input");
                     
+         //Llamando al metodo eliminar que se encuentra en la clase Usuario 
         $datosArray = $_usuario->Eliminar($conn,$postBody);
 
+         // Si existe algun error al eliminar la categoria
         if (isset($datosArray["result"]["error_id"])) {
             // code...
             $responseCode = $datosArray["result"]["error_id"];
@@ -76,6 +84,7 @@ switch($_SERVER['REQUEST_METHOD']){
             http_response_code(200);
         }
 
+         //Retornar el contenido
         echo json_encode($datosArray);
        break;
     
--- BackEnd/Class/Usuarios/Usuario.php
+++ BackEnd/Class/Usuarios/Usuario.php
@@ -4,7 +4,7 @@
 //Includes
 include('../../Class/Includes/IEntidad.php');
 
-
+//Clase Usuario que implementa la interface
 Class Usuario implements IEntidad{
 
     //Atributos 
@@ -17,97 +17,131 @@ Class Usuario implements IEntidad{
     protected $Contrasena =" ";
     protected $ID_Rol;
     protected $Correo = "";
+    protected $token = "";
 
+    //Metodos
+
+    //Funcion implementada de la Interface        
     public function Guardar($conn,$json)
     { 
-        
+         // Array con datos
         $data = json_decode($json,true);
 
-        if( !isset($data['Nombre']) || !isset($data['Apellido']) || !isset($data['Usuario']) || !isset($data['Contrasena']) || !isset($data['ID_Rol']) || !isset($data['Correo']) ) 
-        {      
-               //Campo requido no enviado     
-               return Respuestas::error_400();
+        //Metodo de la clase token, valida si fue enviado o si es valido
+         $this->token = Token::validarToken($conn,$data);
+
+          //Comprobar si el token existe 
+         if(is_bool($this->token))
+         {
+              //Comprobar si los cambios requeridos fueron enviados
+            if( !isset($data['Nombre']) || !isset($data['Apellido']) || !isset($data['Usuario']) || !isset($data['Contrasena']) || !isset($data['ID_Rol']) || !isset($data['Correo']) ) 
+            {      
+                //Campo requido no enviado     
+                return Respuestas::error_400();
+            }
+            else
+            {
+                    //Campo requido enviados  
+                    $this->Nombre             = $data["Nombre"];
+                    $this->Apellido           = $data["Apellido"];
+                    $this->Usuario            = $data["Usuario"];
+                    $this->Contrasena         = $data["Contrasena"];
+                    $this->ID_Rol             = $data["ID_Rol"];
+                    $this->Correo             = $data["Correo"];
+
+                    
+
+                     //Llamar a la function privada para comprobar si el usuario introducido existe
+                    $comprobaruser = $this->Comprobar_Usuario($conn);
+
+                    //Resultado de llamar la funcion
+                    if($comprobaruser >= 1)
+                    {
+                        //No existe un user con ese nombre
+                            
+                            //Llamar a la function privada para comprobar si el correo introducido existe
+                            $comprobarcorreo = $this->Comprobar_Correo($conn);
+                             
+                             //Resultado de llamar la funcion
+                            if($comprobarcorreo >= 1)
+                            {
+                                  //No existe el correo introducido    
+
+                                  //Llamar a la funcion privada para comprobar si el rol introducido existe
+                                    $comprobarrol = $this-> Comprobar_Rol($conn);
+
+                                    //Resultado de llamar la funcion
+                                    if ($comprobarrol >= 1)
+                                    {
+                                            //Encriptar contrasena
+                                            $this->Contrasena = $this->EncriptarContrasena($this->Contrasena);                      
+                                           
+                                            //Llamar la funcion para insertar el usario
+                                            $resp = $this-> Insert_Usuario($conn);
+
+                                            if($resp)
+                                            {
+                                                //Respuesta de insercion exitosa y ID insertado
+                                                $respuesta['result'] = array(
+                                                    "ID_Usuario" => $resp
+                                                );
+                                                return $respuesta;
+                                            }
+                                            else
+                                            {
+                                                //Error al momento de insertar
+                                                Respuestas::error_500();
+                                            }
+                                    }
+                                    else
+                                    { 
+                                        //Error el rol introducido no existe
+                                        return Respuestas::error_500("El rol introducido no ex valido");
+                                        
+                                    }
+
+                            }
+                            else
+                            {
+                                 //El correo introducido ya esta registrado
+                                return Respuestas::error_500("El correo introducido ya existe");
+                            }
+                    }
+                    else
+                    {
+                        //El usuario introducido ya esta registrado
+                        return  Respuestas::error_500("El Usuario introducido ya existe");
+                    }
+    
+            }
         }
         else
         {
-                //Campo requido enviado  
-                $this->Nombre             = $data["Nombre"];
-                $this->Apellido           = $data["Apellido"];
-                $this->Usuario            = $data["Usuario"];
-                $this->Contrasena         = $data["Contrasena"];
-                $this->ID_Rol             = $data["ID_Rol"];
-                $this->Correo             = $data["Correo"];
-
-                  // Llamar funcion privada Insert_Categoria
-
-
-                  $comprobaruser = $this->Comprobar_Usuario($conn);
-
-                  
-                  if($comprobaruser >= 1)
-                  {
-                        $comprobarcorreo = $this->Comprobar_Correo($conn);
-
-                        if($comprobarcorreo >= 1)
-                        {
-                              
-                                $comprobarrol = $this-> Comprobar_Rol($conn);
-
-                                if ($comprobarrol >= 1)
-                                {
-                                        //Encriptar contrasena
-                                        $this->Contrasena = $this->EncriptarContrasena($this->Contrasena);                      
-                                        $resp = $this-> Insert_Usuario($conn);
-
-                                        if($resp)
-                                        {
-                                            //Respuesta de insercion exitosa y ID insertado
-                                            $respuesta['result'] = array(
-                                                "ID_Usuario" => $resp
-                                            );
-                                            return $respuesta;
-                                        }
-                                        else
-                                        {
-                                            //Error al momento de insertar
-                                            Respuestas::error_500();
-                                        }
-                                }
-                                else
-                                { 
-                                    return Respuestas::error_500("El rol introducido no ex valido");
-                                    
-                                }
-
-                        }
-                        else
-                        {
-                            return Respuestas::error_500("El correo introducido ya existe");
-                        }
-                  }
-                  else
-                  {
-                    return  Respuestas::error_500("El Usuario introducido ya existe");
-                  }
-  
+                //Error en caso de que el token sea invalido o no sea enviado
+                return $this->token;
         }
     }
 
 
  
+    //Funcion privada (query de inserion usario)
     private function Insert_Usuario($conn)
     {
+        //Query
         $query = "INSERT into $this->Tabla(Nombre, Apellido, Usuario,Contrasena,ID_Rol,Correo) values ('$this->Nombre','$this->Apellido','$this->Usuario','$this->Contrasena',$this->ID_Rol,'$this->Correo')";
 
+         //Lamando la funcion que se encuentra en la clase conexion y que es utilizada por la interface
         $id = $conn->nonQueryId($query);
 
         if($id)
         {
+            //Se inserto correctamente
            return $id;
 
         } 
         else
         {
+            //Error al momento de insertar
             return 0;
 
         }
@@ -116,24 +150,58 @@ Class Usuario implements IEntidad{
 
     
 
-
+    //Funcion Implementada de la Interface (Obtener todos los usarios)
     public static function ObtenerTodo($conn,$json)
     {
-          $query = self::Select_Usuario();
-          $datos = $conn->Query($query);
-          return $datos;
+         //Validar token
+         $token = Token::validarToken($conn,$json);
+          
+          if(is_bool($token))
+          {         
+                 //Ejecutar function privada Select_Usuario   
+                    $query = self::Select_Usuario();
+                    $datos = $conn->Query($query);
+
+                    //Retornar datos
+                    return $datos;
+          }
+          else
+          {
+               //Error en caso de que el token sea invalido o no sea enviado
+               return  $token;
+          }          
     }
 
+    //Funcion Implementada de la Interface (Obtener un usuario especificado por el ID_Usuario)
     public static function Obtener($conn,$json)
     {
-        $id = $json["ID_Usuario"];
-        $query = self::Select_Usuario(). ' WHERE u.ID_Usuario = '. $id;
-        $datos = $conn->Query($query);
-        return $datos;
+          //Validar token
+          $token = Token::validarToken($conn,$json);
+
+          if(is_bool($token))
+          {
+               //Extrayendo el ID_Usuario solicitado
+            $id = $json["ID_Usuario"];
+
+            //Llamando la funcion privada Select_Categoria con la condicion del usuario solicitada
+            $query = self::Select_Usuario(). ' WHERE u.ID_Usuario = '. $id;
+            $datos = $conn->Query($query);
+         
+            //Retornar datos
+            return $datos;
+          }
+          else
+          {
+                //Error en caso de que el token sea invalido o no sea enviado
+                return  $token;
+
+          } 
     }
 
+    //Funcion privada (Query general para el select de Usuario)
     private static function Select_Usuario()
     {
+        //Query
          $query = "select u.ID_Usuario,u.Nombre, u.Apellido, u.Usuario, r.Nombre Rol, u.Correo, u.Contrasena
                   from Usuario u
                   JOIN Rol r on (u.ID_Rol=r.ID_Rol)";
@@ -142,52 +210,70 @@ Class Usuario implements IEntidad{
     }
 
 
-
+    //Funcion actualizar 
     public function Actualizar($conn,$json)
     {
-      
+      //inhabilitada POR AHORA
 
     }
 
- 
+   //Funcion Implementada de la Interface (Eliminar una Usuario)
     public function Eliminar($conn,$json)
     {
+        // Array con datos
         $data = json_decode($json,true);
+      
 
-        //Comprobar campos requeridos
-       if( !isset($data['ID_Usuario']))
-       {      
-              //Campo requido no enviado     
-              return Respuestas::error_400();
-       }
-       else
-       {
-           //Campo requido enviado  
-             $this->ID_Usuario        = $data['ID_Usuario'];
-           
-           // Llamar funcion privada Insert_Categoria
-           $ID = self::Obtener($conn,$data);
+         //Validar token
+        $this->token = Token::validarToken($conn,$data);
 
-           if($ID)
-           {
-                //EXISTE
-                $resp = $this->Delete_Usuario($conn);
-               
-                 $respuesta = Respuestas::$response;
-                 $respuesta['result'] = array(
-                     "ID_Usuario" => $this->ID_Usuario  
-                 );
-                 return $respuesta;
-                  
-           }
-           else
-           {
-               //Error al momento de insertar
-               return  Respuestas::error_500();
-           }
-       }
+
+        if(is_bool($this->token))
+        {
+            //Comprobar campos requeridos enviados
+            if( !isset($data['ID_Usuario']))
+            {      
+                    //Campo requido no enviado     
+                    return Respuestas::error_400();
+            }
+            else
+            {
+                //Campo requido enviado  
+                    $this->ID_Usuario        = $data['ID_Usuario'];
+                
+                // Llamar funcion obtener para ver si el user especificado existe
+                $ID = self::Obtener($conn,$data);
+
+
+                if($ID)
+                {
+                        //El ID del usario existe
+
+                        //Llamando a la funcion privada Delete Usuario (Eliminar un usario) 
+                        $resp = $this->Delete_Usuario($conn);
+                    
+                        $respuesta = Respuestas::$response;
+                        $respuesta['result'] = array(
+                            "ID_Usuario" => $this->ID_Usuario  
+                        );
+                        return $respuesta;
+                        
+                }
+                else
+                {
+                    //Error al momento de insertar
+                    return  Respuestas::error_500();
+                }
+            }
+        }
+        else
+        {
+            //Error en caso de que el token sea invalido o no sea enviado
+            return $this->token;
+        }    
     }
 
+    //Funcion privada (Delete_Usuario)
     private function Delete_Usuario($conn)
     {
            $query = "DELETE FROM $this->Tabla where ID_Usuario = $this->ID_Usuario";
