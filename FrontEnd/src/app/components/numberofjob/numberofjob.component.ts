858c77209488f141c55121ca28569b11920915d6
LuisPilier <70179807+LuisPilier@users.noreply.github.com>
2021-02-18 18:41:36 -0400

    Adding component numberofjobs

---
+++ FrontEnd/src/app/components/numberofjob/numberofjob.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numberofjob',
+  templateUrl: './numberofjob.component.html',
+  styleUrls: ['./numberofjob.component.css']
+})
+export class NumberofjobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
