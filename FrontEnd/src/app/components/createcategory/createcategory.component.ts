1286c2ee91ad80f689e7d21c63441ac9e6acdefc
LuisPilier <70179807+LuisPilier@users.noreply.github.com>
2021-02-17 20:16:55 -0400

    adding modals

---
+++ FrontEnd/src/app/components/createcategory/createcategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createcategory',
+  templateUrl: './createcategory.component.html',
+  styleUrls: ['./createcategory.component.css']
+})
+export class CreatecategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
