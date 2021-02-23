88a81e31ea7264023fb00a3567f0059c39b5a5c0
LuisPilier <70179807+LuisPilier@users.noreply.github.com>
2021-02-23 00:51:54 -0400

    login 90% finished

--- FrontEnd/src/app/components/homepage/homepage.component.ts
+++ FrontEnd/src/app/components/homepage/homepage.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import{ ApisService } from 'src/app/services/apis.service';
 import {LoginI} from 'src/app/models/login.interface';
+import {ResponseI} from 'src/app/models/response.interface';
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -12,18 +14,35 @@ export class HomepageComponent implements OnInit {
 
 loginForm = new FormGroup({
 Usuario : new FormControl('',Validators.required),
-Pass : new FormControl('',Validators.required),
+Contrasena : new FormControl('',Validators.required),
 });
   
 
-  constructor(private api: ApisService) { }
+  constructor(private api: ApisService, private router:Router) { }
 
+
+  errorMessage:any = "";
+  errorStatus:boolean = false;
   ngOnInit(): void {
+    this.ValidarLocalStorage();
+  }
+
+  ValidarLocalStorage(){
+    if(localStorage.getItem('Token')){
+    document.location.href = (`http://localhost:4200/adminpage`);
+    }
   }
 
   onlogin(form:LoginI){
     this.api.loginByUser(form).subscribe(data =>{
-      console.log(data);
+      let dataResponse:ResponseI = data;
+      if(dataResponse.status == "ok"){
+        localStorage.setItem("Token", dataResponse.result.Token);
+        document.location.href = (`http://localhost:4200/adminpage`);
+      }else{
+        this.errorStatus = true;
+        this.errorMessage = dataResponse.result.error_msg;
+      }
     })
   }
  
