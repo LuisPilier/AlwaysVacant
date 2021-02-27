416dc2281ff0ff6e2a4d8ddd4568fd392e7c109d
Naeliza <lizcespedes0326@gmail.com>
2021-02-27 03:15:59 -0400

    Adding pagination to createcategory

--- FrontEnd/src/app/components/createcategory/createcategory.component.ts
+++ FrontEnd/src/app/components/createcategory/createcategory.component.ts
@@ -31,6 +31,7 @@ export class CreatecategoryComponent implements OnInit {
 
   constructor(private http: HttpClient, private api: ApisService) { }
   conversion: [] = [];
+  paginactual: number = 1;
   
     
 
