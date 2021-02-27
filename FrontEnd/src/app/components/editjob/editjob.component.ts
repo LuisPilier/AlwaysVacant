5e7a54a6fb6ef29e95b8ec2a4a33e246881c930a
AVCode <38996897+advr0106@users.noreply.github.com>
2021-02-27 01:51:09 -0400

    .

--- FrontEnd/src/app/components/editjob/editjob.component.ts
+++ FrontEnd/src/app/components/editjob/editjob.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApisService } from 'src/app/services/apis.service';
-import { Router } from '@angular/router'
+import {Router} from '@angular/router'
+
+
+
+
+
 @Component({
   selector: 'app-editjob',
   templateUrl: './editjob.component.html',
   styleUrls: ['./editjob.component.css']
 })
 export class EditjobComponent implements OnInit {
-
+  
 
 
   constructor(private http: HttpClient) { }
   conversion: [] = [];
-
-
+ 
+ 
 
   ngOnInit(): void {
-
+   this.getData();
   }
+  getData(){
+    this.http.get('https://en-linea.app/AlwaysVacant/BackEnd/API/vacante.php')
+    .subscribe((data:any) => { 
+     this.conversion=data;
+     console.log( this.conversion );
+    });
+  }
+  
+  
+  
+
 }
