001c34d85a2197959fb4a6a3f7f5c6549f323c18
Naeliza <lizcespedes0326@gmail.com>
2021-02-27 02:42:00 -0400

    Conecting homepagejob's table to the API

--- FrontEnd/src/app/components/homepagejobs/homepagejobs.component.ts
+++ FrontEnd/src/app/components/homepagejobs/homepagejobs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepagejobs',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagejobsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+    conversion: [] = []; 
+  
 
   ngOnInit(): void {
+  this.getData();
   }
 
+  getData(){
+    this.http.get('https://en-linea.app/AlwaysVacant/BackEnd/API/vacante.php')
+    .subscribe((data:any) => { 
+     this.conversion=data;
+     console.log( this.conversion );
+    });
+  }
 }
