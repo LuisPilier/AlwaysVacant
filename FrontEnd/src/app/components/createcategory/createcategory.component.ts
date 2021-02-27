53b7775527c40848e3f4c01d8c86828150999340
AVCode <38996897+advr0106@users.noreply.github.com>
2021-02-27 19:28:09 -0400

    combo

--- FrontEnd/src/app/components/createcategory/createcategory.component.ts
+++ FrontEnd/src/app/components/createcategory/createcategory.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import{ ApisService } from 'src/app/services/apis.service';
-import {ResponseI} from 'src/app/models/response.interface';
-import {CategoryI} from 'src/app/models/category.interface';
+import { ApisService } from 'src/app/services/apis.service';
+import { ResponseI } from 'src/app/models/response.interface';
+import { CategoryI } from 'src/app/models/category.interface';
 @Component({
   selector: 'app-createcategory',
   templateUrl: './createcategory.component.html',
@@ -11,83 +11,69 @@ import {CategoryI} from 'src/app/models/category.interface';
 })
 export class CreatecategoryComponent implements OnInit {
 
-  
-
   nuevoForm = new FormGroup({
-    Nombre : new FormControl('',Validators.required),
-    Token : new FormControl('', Validators.required)
-    });
+    Nombre: new FormControl('', Validators.required),
+    Token: new FormControl('', Validators.required)
+  });
 
-    editarForm = new FormGroup({
-      Nombre : new FormControl(''),
-      ID_Categoria : new FormControl(''),
-      Token : new FormControl('')
-    });
-  
+  editarForm = new FormGroup({
+    Nombre: new FormControl(''),
+    ID_Categoria: new FormControl(''),
+    Token: new FormControl('')
+  });
 
-    
-    public valueSelected: any
-    public valueSelectedid: any
+  public valueSelected: any
+  public valueSelectedid: any
 
   constructor(private http: HttpClient, private api: ApisService) { }
   conversion: [] = [];
-  
-    
-
 
   ngOnInit(): void {
     this.getData();
     let Token = localStorage.getItem('Token');
     this.nuevoForm.patchValue({
-     'Token' : Token
+      'Token': Token
     });
 
     let Tokenn = localStorage.getItem('Token');
     this.editarForm.patchValue({
-     'Token' : Token
-    });
-}
-
-
-
-  getData(){
-    this.http.get('https://en-linea.app/AlwaysVacant/BackEnd/API/categoria.php')
-    .subscribe((data:any) => { 
-     this.conversion=data;
-     console.log( this.conversion );
+      'Token': Token
     });
   }
 
-  postForm(form:CategoryI){
-    this.api.postCategory(form).subscribe(data =>{
+  getData() {
+    this.http.get('https://en-linea.app/AlwaysVacant/BackEnd/API/categoria.php')
+      .subscribe((data: any) => {
+        this.conversion = data;
+        console.log(this.conversion);
+      });
+  }
+
+  postForm(form: CategoryI) {
+    this.api.postCategory(form).subscribe(data => {
       console.log(data);
       document.location.href = (`http://localhost:4200/createcategory`);
     })
-}
+  }
 
-putForm(form:CategoryI){
+  putForm(form: CategoryI) {
 
-  this.api.putCategory(form).subscribe(data =>{
-    console.log(data);
-    document.location.href = (`http://localhost:4200/createcategory`);
-  })
-}
+    this.api.putCategory(form).subscribe(data => {
+      console.log(data);
+      document.location.href = (`http://localhost:4200/createcategory`);
+    })
+  }
 
- deleteForm(){
-   let datos:CategoryI = this.editarForm.value;
-   this.api.deleteCategory(datos).subscribe(data =>{
-    document.location.href = (`http://localhost:4200/createcategory`);
-    console.log(data);
+  deleteForm() {
+    let datos: CategoryI = this.editarForm.value;
+    this.api.deleteCategory(datos).subscribe(data => {
+      document.location.href = (`http://localhost:4200/createcategory`);
+      console.log(data);
+    })
+  }
 
-   })
- }
-
-seleccionarCategoria(categoria: HTMLInputElement,id: HTMLInputElement  ){
-this.valueSelected = categoria.value
-this.valueSelectedid = id.value
-}
-
-
- 
-
+  seleccionarCategoria(categoria: HTMLInputElement, id: HTMLInputElement) {
+    this.valueSelected = categoria.value
+    this.valueSelectedid = id.value
+  }
 }
--- FrontEnd/src/app/components/homepagejobs/homepagejobs.component.ts
+++ FrontEnd/src/app/components/homepagejobs/homepagejobs.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApisService } from 'src/app/services/apis.service';
+import { ResponseI } from 'src/app/models/response.interface';
+import { CategoryI } from 'src/app/models/category.interface';
 @Component({
   selector: 'app-homepagejobs',
   templateUrl: './homepagejobs.component.html',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepagejobsComponent implements OnInit {
 
-  constructor() { }
+  nuevoForm = new FormGroup({
+    Nombre: new FormControl('', Validators.required),
+    Token: new FormControl('', Validators.required)
+  });
+  constructor(private http: HttpClient, private api: ApisService) { }
+  conversion: [] = [];
 
   ngOnInit(): void {
-  }
+    this.getData();
+    let Token = localStorage.getItem('Token');
+    this.nuevoForm.patchValue({
+      'Token': Token
+    });
 
+  }
+  getData() {
+    this.http.get('https://en-linea.app/AlwaysVacant/BackEnd/API/categoria.php')
+      .subscribe((data: any) => {
+        this.conversion = data;
+        console.log(this.conversion);
+      });
+  }
 }
