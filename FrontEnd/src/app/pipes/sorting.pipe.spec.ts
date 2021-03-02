f8620f0760b39fde762535d232a30c1345baccb1
Naeliza <lizcespedes0326@gmail.com>
2021-03-02 09:37:23 -0400

    Doing a Sorting Pipe

---
+++ FrontEnd/src/app/pipes/sorting.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortingPipe } from './sorting.pipe';
+
+describe('SortingPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortingPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
